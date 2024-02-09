4b81e131dc9a8bf7b2aa3eef72d2074bfb42235f
Jacob <jgharris314@gmail.com>
2024-02-09 13:06:26 -0700

    implemented data structure with book title, author, and link to amazon. todo for data: add image hrefs

---
+++ app/types.ts
@@ -0,0 +1,10 @@
+type MediaType = "audible" | "kindle" | "paperback" | "hardcover"
+
+export interface Book {
+  title: string
+  img: string
+  author: string
+  amazonLink: string
+  mediaType: MediaType
+  isPromoted: boolean
+}
